436b5a3f876df31149b8be96464acae7bb0d4011
a4Anubhav <anubhavmalik.dx@gmail.com>
2025-09-04 18:32:08 +0530

    UI improbvements V1.3

--- src/Component/FreeUser/FreeUser.js
+++ src/Component/FreeUser/FreeUser.js
@@ -12,8 +12,38 @@ const FreeUser = () => {
         </div>
       </div>
       <div className={styles.LeftSideSection}>
-        <div className={styles.TextFreeUserSection}></div>
-        <div className={styles.CTASection}></div>
+        <div className={styles.TextFreeUserSection}>
+          <h1>
+            Get Your <span className={styles.FreeText}>FREE</span>
+          </h1>
+          <h2>
+            AI Receptionist{" "}
+            <span className={styles.INText}>
+              in <b>just 3 minutes</b>
+            </span>
+          </h2>
+        </div>
+        <div className={styles.CTASection}>
+          <div className={styles.CTATextSection}>
+            <p>Join the Revolution & Transform</p>
+            <ul>
+              <li>
+                Answer calls <b>24/7</b>- Even when you’re away
+              </li>
+              <li>
+                Schedule appointments <b>automatically</b>
+              </li>
+              <li>
+                <b>Nurture leads</b> and boost sales
+              </li>
+            </ul>
+          </div>
+          <div className={styles.CTAVideo}>
+            <div className={styles.VideoBox}>
+                <img src="/Images/CTA-Video.png" alt="ViewDemo"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
