212c91177b4b283364a7df30276c52d52a163193
a4Anubhav <anubhavmalik.dx@gmail.com>
2025-09-04 13:03:46 +0530

    UI improbvements V1.2

--- src/Component/FreeUser/FreeUser.js
+++ src/Component/FreeUser/FreeUser.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import styles from "../FreeUser/FreeUser.module.css"
+import React from "react";
+import styles from "../FreeUser/FreeUser.module.css";
 
 const FreeUser = () => {
-    return (
-        <div className={styles.containerBox}>
-            <div className={styles.container}>
-                <div className={styles.ShapeDiv}>
-                    <div className={styles.gifDiv}>
-                        <img src="Images/RexptGif.gif" alt="RexptGif" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.containerBox}>
+      <div className={styles.container}>
+        <div className={styles.ShapeDiv}>
+          <div className={styles.gifDiv}>
+            <img src="Images/RexptGif.gif" alt="RexptGif" />
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className={styles.LeftSideSection}>
+        <div className={styles.TextFreeUserSection}></div>
+        <div className={styles.CTASection}></div>
+      </div>
+    </div>
+  );
+};
 
-export default FreeUser
+export default FreeUser;
