97cba3736be6dfc2a3a1ca299dfa79f5b2c32221
NirajDX <niraj.t@designersx.com>
2025-09-04 11:53:33 +0530

    Free User Code Latest v1.1 9.04.2025

---
+++ src/Component/FreeUser/FreeUser.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "../FreeUser/FreeUser.module.css"
+
+const FreeUser = () => {
+    return (
+        <div className={styles.containerBox}>
+            <div className={styles.container}>
+                <div className={styles.ShapeDiv}>
+                    <div className={styles.gifDiv}>
+                        <img src="Images/RexptGif.gif" alt="RexptGif" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FreeUser
---
+++ src/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FreeUser from '../../Component/FreeUser/FreeUser'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <FreeUser/>
+    </div>
+  )
+}
+
+export default LandingPage
