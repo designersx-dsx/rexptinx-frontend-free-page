cb9f827511f623f52f78320eb10d68973f0dd592
NirajDX <niraj.t@designersx.com>
2025-09-09 16:47:30 +0530

    [AP] Ui new section and animation add (AP v1.1.1.)

---
+++ src/Component/GetInTouch/GetInTouch.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from '../GetInTouch/GetInTouch.module.css'
+
+const GetInTouch = () => {
+    return (
+        <div className={styles.containerBox}>
+            <div className={styles.container}>
+                <div className={styles.section}>
+                    <div className={styles.LeftSection}>
+                        <h1 className={styles.subTitle}>Let's Get In Touch.</h1>
+                        <p className={styles.description}>Ready to stop missing calls and start growing your business?</p>
+
+                        <div className={styles.joinImg}>
+                            <img src='Svg/join-rexpt-white.svg' alt='join-rexpt-white.svg' />
+                        </div>
+
+                    </div>
+                 <div className={styles.RightSection}>
+    <div className={styles.waveWrapper}>
+        <div className={styles.waveDive}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <img src="Svg/logo-icon.svg" alt="logo-icon" className={styles.logoIcon} />
+    </div>
+</div>
+
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GetInTouch
